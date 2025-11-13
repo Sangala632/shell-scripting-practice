8c317a102f91b780e1cf2a867be7907f774b94b4
sangala632 <sangala2003@gmail.com>
2025-11-13 14:33:12 +0530

    added some script files

--- 5-varibles.sh
+++ 5-varibles.sh
@@ -3,4 +3,4 @@ echo "enter the pin::"
 
 read -s PIN # -s option is used to hide the input while typing and pin is the variable name
 
-echo "entered pin is : $PIN"
+echo "your pin numbers is : $PIN"
