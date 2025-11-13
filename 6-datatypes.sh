fe116d67c033265af5b428e8e4ad06e7f4356b93
sangala632 <sangala2003@gmail.com>
2025-11-13 14:49:39 +0530

    added some script files

---
+++ 6-datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "print time and date"
+
+TIMEDATE=$(date)
+echo "current date and time is :: $TIMEDATE"
+Number1=10
+Number2=20
+sum_of_numbers=$((Number1 + Number2))
+echo "sum of numbers is :: $sum_of_numbers"
