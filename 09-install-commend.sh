2aa541109814a0fb43798e19d4ab43576d257875
sangala632 <sangala2003@gmail.com>
2025-11-14 19:44:18 +0530

    arrays

--- 09-install-commend.sh
+++ 09-install-commend.sh
@@ -2,10 +2,17 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-echo "Error :: please run the script with sudo or root user aceess"
+    echo "Error :: please run the script with sudo or root user aceess"
 exit 1
 else 
-echo "Sucess :: you are running the script with sudo or root user access"
+    echo "Sucess :: you are running the script with sudo or root user access"
 
 dnf install mysql-server -y
+
+if [ $? -eq 0 ]
+then
+    echo "Mysql server installed successfully"
+    else
+    echo "Error :: Mysql server installation failed"
+        exit 1
 fi
