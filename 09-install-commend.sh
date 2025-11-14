732e3baa3e7edddb68a6235fbcbd7b15f0b5eb90
sangala632 <sangala2003@gmail.com>
2025-11-14 20:08:06 +0530

    arrays

--- 09-install-commend.sh
+++ 09-install-commend.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
+
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Error :: please run the script with sudo or root user aceess"
-exit 1
+    exit 1
 else 
     echo "Sucess :: you are running the script with sudo or root user access"
 fi
 
 dnf list installed  mysql
 
+# checking alerady mysql is installed or not.if installed $? is 0, then
+# if not installed $? is not o .expression is true
 if [ $? -ne 0 ]
 then
     echo "Mysql is not installed... going to  install"
     dnf install mysql -y
-if [ $? -eq 0 ]
-then
-    echo "installing the mysql server is sucess"
+    if [ $? -eq 0 ]
+    then
+        echo "installing the mysql server is sucess"
     else
-    echo "Error :: Mysql server installation failed"
-    exit 1
-fi
+        echo "Error :: Mysql server installation failed"
+        exit 1
+    fi
 else 
-    echo "my sql is alerady installed in the syatem.. nothing to do
+    echo "my sql is alerady installed in the syatem.. nothing to do"
 fi
